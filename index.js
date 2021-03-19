51be53eee9fa49b82c543b35d56c4233d7e6672e
Sunitha Iyadurai <sunithaiyadurai@gmail.com>
2021-03-19 19:59:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function reverse(str) {
+  let lettersArray = str.split('');
+  let reversedLettersArray = lettersArray.reverse();
+  return reversedLettersArray.join('');
+}
+console.log(reverse("KLKL"))
+
+function reverse(str1) {
+   return str1.split('').reverse().join('');
+}
+
+function isPalindrome(str1) {
+   return reverse(str1) === str1;
+}
+console.log(reverse("Vigi"))
